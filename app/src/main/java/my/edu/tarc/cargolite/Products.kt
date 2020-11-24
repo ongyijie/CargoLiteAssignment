44ededfd9df9e15f88d104bd0434146e7e5bdef6
Ong Yi Jie <ongyj-wp18@student.tarc.edu.my>
2020-11-25 00:37:48 +0800

    24/11 12.37pm commit (write to database done)

--- app/src/main/java/my/edu/tarc/cargolite/Products.kt
+++ app/src/main/java/my/edu/tarc/cargolite/Products.kt
@@ -3,18 +3,16 @@ package my.edu.tarc.cargolite
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.dialog_addproduct.view.*
 
 
-class Products: AppCompatActivity() {
+class Products : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +29,13 @@ class Products: AppCompatActivity() {
             val DialogView = LayoutInflater.from(this).inflate(R.layout.dialog_addproduct, null)
             //AlertDialogBuilder
             val myBuilder = AlertDialog.Builder(this)
-                    .setView(DialogView)
-                    .setTitle("Add New Product")
+                .setView(DialogView)
+                .setTitle("Add New Product")
             //Show dialog
-            val  mAlertDialog = myBuilder.show()
-            val productList = ArrayList<String>(5)
+            val mAlertDialog = myBuilder.show()
 
-            val textViewProducts: TextView = findViewById(R.id.textViewProducts)
+            //val textViewProducts: TextView = findViewById(R.id.textViewProducts)
+
             //Add button click of custom layout
             DialogView.dialogAddBtn.setOnClickListener {
                 //Dismiss dialog
@@ -66,30 +64,18 @@ class Products: AppCompatActivity() {
                     "stockLocation" to stockLocation,
                     "quantity" to quantity
                 )
-
-                database.collection("products")
-                        .add(product)
-                        .addOnSuccessListener { documentReference ->
-                        Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
-                        }
-                        .addOnFailureListener { e ->
+                database.collection("products").document("$productID")
+                    .set(product)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("TAG", "DocumentSnapshot added")
+                    }
+                    .addOnFailureListener { e ->
                         Log.w("TAG", "Error adding document", e)
-                        }
-                textViewProducts.text = String.format(
-                    "%20s %10s \n%20s %10s \n%20s %10s \n%20s %10s \n%20s %10s",
-                    "Product ID: ",
-                    productID,
-                    "Product Name: ",
-                    productName,
-                    "Unit price (RM) :",
-                    unitPrice,
-                    "Stock location :",
-                    stockLocation,
-                    "Quantity: ",
-                    quantity
-                )
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.d("TAG", "get failed with ", exception)
+                    }
             }
-
             //Cancel button click of custom layout
             DialogView.dialogCancelBtn.setOnClickListener {
                 //Dismiss dialog
