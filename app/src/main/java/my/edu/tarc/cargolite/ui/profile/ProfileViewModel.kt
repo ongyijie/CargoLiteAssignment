07105876f90a597a562b1ac729222a431cb4c665
CHAN-MH <chanmh-wp18@student.tarc.edu.my>
2020-11-24 15:57:19 +0800

    settle login bug and User Profile UI

--- app/src/main/java/my/edu/tarc/cargolite/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/my/edu/tarc/cargolite/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,15 @@ package my.edu.tarc.cargolite.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import my.edu.tarc.cargolite.R
 
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+        //value = ""
     }
     val text: LiveData<String> = _text
+
+
 }
