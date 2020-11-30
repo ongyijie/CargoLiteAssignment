4597618cfb2a17d344b38666317d65b16e2fd09b
Ong Yi Jie <ongyj-wp18@student.tarc.edu.my>
2020-11-30 18:39:49 +0800

    products and settings done

--- app/src/main/java/my/edu/tarc/cargolite/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/cargolite/MainActivity.kt
@@ -1,12 +1,18 @@
 package my.edu.tarc.cargolite
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +26,42 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications, R.id.navigation_settings))
+                R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                val builder = AlertDialog.Builder(this)
+                builder.setCancelable(false)
+                builder.setTitle(R.string.dialogLogout)
+                builder.setMessage(R.string.messageLogout)
+                builder.setPositiveButton("Yes") { dialog, which ->
+
+                    FirebaseAuth.getInstance().signOut()
+                    val intentLogin = Intent(this, Login::class.java)
+                    intentLogin.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK    //Prevent back button after logout
+                    startActivity(intentLogin)
+                }
+                builder.setNegativeButton("No") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
